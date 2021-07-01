fe95018339f433e6403c284f583950ecd0daf48b
Arthur Rambo <arthurbrachtrambo@gmail.com>
2021-06-30 21:51:48 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaCadastroAluno/CadastroDoAluno.Designer.cs
@@ -0,0 +1,381 @@
+﻿
+namespace SistemaCadastroAluno
+{
+    partial class CadastroDoAluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnFem = new System.Windows.Forms.RadioButton();
+            this.btnMasc = new System.Windows.Forms.RadioButton();
+            this.CPFResp1 = new System.Windows.Forms.MaskedTextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.CPFResp2 = new System.Windows.Forms.MaskedTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNomeAluno = new System.Windows.Forms.TextBox();
+            this.txtNomeResp1 = new System.Windows.Forms.TextBox();
+            this.txtNomeResp2 = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.ComboBox();
+            this.listAlunos = new System.Windows.Forms.ListBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnOutro = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome completo do aluno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data de nascimento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nome completo ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 415);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Nome completo";
+            // 
+            // dataNascimento
+            // 
+            this.dataNascimento.Location = new System.Drawing.Point(225, 177);
+            this.dataNascimento.Name = "dataNascimento";
+            this.dataNascimento.Size = new System.Drawing.Size(200, 23);
+            this.dataNascimento.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Dados do aluno:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 291);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(131, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Dados do responsável 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(150, 218);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 15);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Ano escolar";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(182, 245);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Sexo";
+            // 
+            // btnFem
+            // 
+            this.btnFem.AutoSize = true;
+            this.btnFem.Location = new System.Drawing.Point(226, 245);
+            this.btnFem.Name = "btnFem";
+            this.btnFem.Size = new System.Drawing.Size(75, 19);
+            this.btnFem.TabIndex = 16;
+            this.btnFem.TabStop = true;
+            this.btnFem.Text = "Feminino";
+            this.btnFem.UseVisualStyleBackColor = true;
+            // 
+            // btnMasc
+            // 
+            this.btnMasc.AutoSize = true;
+            this.btnMasc.Location = new System.Drawing.Point(307, 245);
+            this.btnMasc.Name = "btnMasc";
+            this.btnMasc.Size = new System.Drawing.Size(80, 19);
+            this.btnMasc.TabIndex = 17;
+            this.btnMasc.TabStop = true;
+            this.btnMasc.Text = "Masculino";
+            this.btnMasc.UseVisualStyleBackColor = true;
+            // 
+            // CPFResp1
+            // 
+            this.CPFResp1.Location = new System.Drawing.Point(225, 342);
+            this.CPFResp1.Mask = "999.999.999-99";
+            this.CPFResp1.Name = "CPFResp1";
+            this.CPFResp1.Size = new System.Drawing.Size(100, 23);
+            this.CPFResp1.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(181, 345);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 15);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "CPF";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(62, 389);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(131, 15);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Dados do responsável 2";
+            // 
+            // CPFResp2
+            // 
+            this.CPFResp2.Location = new System.Drawing.Point(225, 441);
+            this.CPFResp2.Mask = "999.999.999-99";
+            this.CPFResp2.Name = "CPFResp2";
+            this.CPFResp2.Size = new System.Drawing.Size(100, 23);
+            this.CPFResp2.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(191, 444);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(28, 15);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "CPF";
+            // 
+            // txtNomeAluno
+            // 
+            this.txtNomeAluno.Location = new System.Drawing.Point(225, 142);
+            this.txtNomeAluno.Name = "txtNomeAluno";
+            this.txtNomeAluno.Size = new System.Drawing.Size(258, 23);
+            this.txtNomeAluno.TabIndex = 23;
+            // 
+            // txtNomeResp1
+            // 
+            this.txtNomeResp1.Location = new System.Drawing.Point(225, 313);
+            this.txtNomeResp1.Name = "txtNomeResp1";
+            this.txtNomeResp1.Size = new System.Drawing.Size(245, 23);
+            this.txtNomeResp1.TabIndex = 24;
+            // 
+            // txtNomeResp2
+            // 
+            this.txtNomeResp2.Location = new System.Drawing.Point(225, 412);
+            this.txtNomeResp2.Name = "txtNomeResp2";
+            this.txtNomeResp2.Size = new System.Drawing.Size(245, 23);
+            this.txtNomeResp2.TabIndex = 25;
+            // 
+            // txtAno
+            // 
+            this.txtAno.FormattingEnabled = true;
+            this.txtAno.Items.AddRange(new object[] {
+            "1°",
+            "2°",
+            "3°",
+            "4°",
+            "5°",
+            "6°",
+            "7°",
+            "8°",
+            "9°"});
+            this.txtAno.Location = new System.Drawing.Point(225, 215);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(49, 23);
+            this.txtAno.TabIndex = 13;
+            // 
+            // listAlunos
+            // 
+            this.listAlunos.FormattingEnabled = true;
+            this.listAlunos.ItemHeight = 15;
+            this.listAlunos.Location = new System.Drawing.Point(559, 166);
+            this.listAlunos.Name = "listAlunos";
+            this.listAlunos.Size = new System.Drawing.Size(336, 199);
+            this.listAlunos.TabIndex = 26;
+            this.listAlunos.SelectedIndexChanged += new System.EventHandler(this.listAlunos_SelectedIndexChanged);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(559, 142);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(110, 15);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "Alunos cadastrados";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(559, 394);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 28;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(650, 394);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 29;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(740, 394);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 30;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnOutro
+            // 
+            this.btnOutro.AutoSize = true;
+            this.btnOutro.Location = new System.Drawing.Point(393, 245);
+            this.btnOutro.Name = "btnOutro";
+            this.btnOutro.Size = new System.Drawing.Size(56, 19);
+            this.btnOutro.TabIndex = 31;
+            this.btnOutro.TabStop = true;
+            this.btnOutro.Text = "Outro";
+            this.btnOutro.UseVisualStyleBackColor = true;
+            // 
+            // CadastroDoAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 749);
+            this.Controls.Add(this.btnOutro);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.listAlunos);
+            this.Controls.Add(this.txtNomeResp2);
+            this.Controls.Add(this.txtNomeResp1);
+            this.Controls.Add(this.txtNomeAluno);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.CPFResp2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.CPFResp1);
+            this.Controls.Add(this.btnMasc);
+            this.Controls.Add(this.btnFem);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataNascimento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CadastroDoAluno";
+            this.Text = "CadastroDoAluno";
+            this.Load += new System.EventHandler(this.CadastroDoAluno_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dataNascimento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton btnFem;
+        private System.Windows.Forms.RadioButton btnMasc;
+        private System.Windows.Forms.MaskedTextBox CPFResp1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.MaskedTextBox CPFResp2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtNomeAluno;
+        private System.Windows.Forms.TextBox txtNomeResp1;
+        private System.Windows.Forms.TextBox txtNomeResp2;
+        private System.Windows.Forms.ComboBox txtAno;
+        private System.Windows.Forms.ListBox listAlunos;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.RadioButton btnOutro;
+    }
+}
---
+++ SistemaCadastroAluno/CadastroDoAluno.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SistemaCadastroAluno
+{
+    public partial class CadastroDoAluno : Form
+    {
+        public CadastroDoAluno()
+        {
+            InitializeComponent();
+        }
+        List<string> listaNomes = new List<string>();
+        List<DateTime> listaDataNascimento = new List<DateTime>();
+        List<string> listaAno = new List<string>();
+        List<char> listaSexo = new List<char>();
+        List<string> listaNomeResp1 = new List<string>();
+        List<string> listaCPFResp1 = new List<string>();
+        List<string> listaNomeResp2 = new List<string>();
+        List<string> listaCPFResp2 = new List<string>();
+        bool novo;
+
+        private void CadastroDoAluno_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (listaNomes.Contains(txtNomeAluno.Text))
+            {
+                novo = false;
+                
+            }
+            else
+            {
+                novo = true;
+                
+            }
+            if (novo)
+            {
+                if (TudoPreenchido())
+                {
+                    SalvarDados();
+                    listAlunos.Items.Add(txtNomeAluno.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Preencha todos os campos para realizar o cadastro.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Limpe os dados para cadastrar um novo aluno.");
+            }
+        }
+        private bool TudoPreenchido()
+        {
+            
+            if (!String.IsNullOrEmpty(txtNomeAluno.Text) && !String.IsNullOrEmpty(dataNascimento.Text) &&
+                !String.IsNullOrEmpty(txtAno.Text))
+            {
+                if (txtNomeResp1.Text == "" && txtNomeResp2.Text == "" && CPFResp1.Text == "" && CPFResp2.Text == "")
+                {
+                    return false;
+                        }
+                else {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void SalvarDados()
+        {
+            listaNomes.Add(txtNomeAluno.Text);
+            listaDataNascimento.Add(dataNascimento.Value);
+            listaAno.Add(txtAno.Text.ToString());
+            if (btnFem.Checked)
+            {
+                listaSexo.Add('F');
+
+            }
+            else if (btnMasc.Checked)
+            {
+                listaSexo.Add('M');
+
+            }
+            else if (btnOutro.Checked)
+            {
+                listaSexo.Add('X');
+
+            }
+
+            listaNomeResp1.Add(txtNomeResp1.Text);
+            listaNomeResp2.Add(txtNomeResp2.Text);
+            listaCPFResp1.Add(CPFResp1.Text);
+            listaCPFResp2.Add(CPFResp2.Text);
+
+
+        }
+        private void listAlunos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNomeAluno.Text = listaNomes[listAlunos.SelectedIndex];
+            dataNascimento.Value = listaDataNascimento[listAlunos.SelectedIndex];
+            if (listaSexo[listAlunos.SelectedIndex] == 'F')
+            {
+                btnFem.Checked = true;
+            }
+            else if (listaSexo[listAlunos.SelectedIndex] == 'M')
+            {
+                btnMasc.Checked = true;
+            }
+            else if (listaSexo[listAlunos.SelectedIndex] == 'X')
+            {
+                btnOutro.Checked = true;
+            }
+
+        }
+    }
+}
